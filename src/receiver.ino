79d4f429135ddd65667cb5b42c4d9714d9075bd0
Lavanya572 <110762606+Lavanya572@users.noreply.github.com>
2024-11-08 14:49:29 +0530

    Receiver-code

--- src/receiver.ino
+++ src/receiver.ino
@@ -1,6 +1,6 @@
 #define BLYNK_TEMPLATE_ID "TMPL3JLG0IrHM"
-#define BLYNK_TEMPLATE_NAME "Quickstart Template"
-#define BLYNK_AUTH_TOKEN "a7loU68cogNMZzv2SMupfLxDqTEDDY3Y"
+#define BLYNK_TEMPLATE_NAME "Template"
+#define BLYNK_AUTH_TOKEN "a7loU68cogMMZzv2kkhupfLxDedTEDDY3Y"
 #define BLYNK_PRINT Serial
 
 #include <SPI.h>
@@ -13,8 +13,8 @@
 #define RST 5
 #define DI0 2
 
-char ssid[] = "MilKyWay";
-char pass[] = "MILKYWAY2002";
+char ssid[] = "SSIDWIFI";
+char pass[] = "PasswordWiFi";
 
 BlynkTimer timer;
 
@@ -23,33 +23,9 @@ String LoRaData;
 BLYNK_WRITE(V0)
 {
   int value = param.asInt();
-  Blynk.virtualWrite(V0, value);
+  Blynk.write(V0, value);
 }
 
-// void sendSensor() {
-//   int packetSize = LoRa.parsePacket();
-  
-//   if (packetSize) {
-//     Serial.print("Received packet: ");
-    
-//     while (LoRa.available()) {
-//       LoRaData = LoRa.readString();
-//       Serial.println("LoRa Data: " + LoRaData);
-
-//       // Extract the numeric value from the LoRaData string
-//       int receivedDistance = LoRaData.substring(15, LoRaData.indexOf(" cm")).toInt();
-//       // Adjust substring indices based on the actual format
-//       Serial.println("Received Distance: " + String(receivedDistance));
-
-//       // Send the extracted data to Blynk with proper formatting
-//       Blynk.virtualWrite(V0, "Water Depth: " + String(receivedDistance) + " cm");
-//       // Send the extracted data to Blynk
-//       // Blynk.virtualWrite(V0, receivedDistance);
-//     }
-
-//     Serial.println("End of packet.");
-//   }
-// }
 
 // new void sensor data
 void sendSensor() {
@@ -67,11 +43,11 @@ void sendSensor() {
       int receivedDistance = 0;
 
       if (indexOfCm != -1) {
-        int startIdx = 13; // Adjust based on the actual format
-        int endIdx = indexOfCm;
+        int startInx = 13; // Adjust based on the actual format
+        int endInx = indexOfCm;
         String distanceStr = LoRaData.substring(startIdx, endIdx);
-        receivedDistance = distanceStr.toInt();
-        Serial.println("Received Distance: " + String(receivedDistance));
+        receivedDist = distanceStr.toInt();
+        Serial.println("Recieved Distance: " + String(recievedDist));
       } else {
         Serial.println("Error: 'cm' not found in LoRaData");
       }
@@ -80,7 +56,7 @@ void sendSensor() {
       unsigned long currentMillis = millis();
 
       // Send the extracted data to Blynk with proper formatting
-      Blynk.virtualWrite(V0, "Water Depth: " + String(receivedDistance) + " cm - " + String(currentMillis));
+      Blynk.write(V0, "Water Depth: " + String(receivedDistance) + " cm - " + String(currentMillis));
       delay(1000);  // Adjust the delay as needed
     }
 
@@ -102,11 +78,10 @@ void setup() {
     delay(500);
   }
 
-  LoRa.setSyncWord(0xF1);
   Serial.println("LoRa Initializing Successful!");
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  timer.setInterval(1000L, sendSensor);
+  timer.setInterval(100L, sendSensor);
 }
 
 void loop() {
